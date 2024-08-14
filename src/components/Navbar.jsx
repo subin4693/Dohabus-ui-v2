b049327d5fe3262bf352114141845f61aed686a3
subin4693 <46subin@gmail.com>
2024-08-14 15:41:56 +0530

    colours html added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import logo from "../assets/logologo.png";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <nav className="bg-yellow-100">
+            <div className="bg-custom-blue w-[100px] h-[100px]"></div>
+            <div className="w-[20mm] bg-custom-yellow">
+                <img src={logo} className="w-full h-full object-cover" />
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
