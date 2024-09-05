f15d6ee457c08b0adf7c204c94d0c42038c5d29f
Rushwan07 <rushdeveloper07@gmail.com>
2024-09-05 11:00:13 +0530

    Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -66,7 +66,7 @@ const Footer = () => {
   };
   return (
     <>
-      <div className="bg-yellow-400 p-10 md:px-20 flex flex-col justify-center items-center ">
+      <div className="bg-yellow-400 p-1 md:px-20 flex flex-col justify-center items-center ">
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 place-items-start text-dark">
           {/* Useful Pages Section */}
           <div className="space-y-5">
@@ -185,7 +185,7 @@ const Footer = () => {
         </div>
 
         {/* Footer Bottom */}
-        <div className="mt-10 text-center text-lg flex justify-between  w-full items-center ">
+        <div className="mt-1 text-center text-lg flex justify-between  w-full items-center ">
           <p>© Copyright 2022 - Doha Bus - All Rights Reserved</p>
 
           <div className="w-[200px] hidden md:inline">
