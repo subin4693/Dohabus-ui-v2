79ef4aa49291858c4cf671e8f1db2929544d8f75
Rushwan07 <rushdeveloper07@gmail.com>
2024-09-21 20:10:18 +0530

    Coupon delete added

--- src/pages/Admin/Offers/OfferCard.jsx
+++ src/pages/Admin/Offers/OfferCard.jsx
@@ -1,54 +1,52 @@
 import React from "react";
+import { BiTrash } from "react-icons/bi";
 
-const OfferCard = ({ offer, handleStatus }) => {
-    // Determine button styles based on the offer status
-    const statusClass =
-        offer.status === "active"
-            ? "bg-green-500 text-white"
-            : "bg-red-500 text-white";
+const OfferCard = ({ offer, handleStatus, handleRemove }) => {
+  // Determine button styles based on the offer status
+  const statusClass =
+    offer.status === "active"
+      ? "bg-green-500 text-white"
+      : "bg-red-500 text-white";
 
-    return (
-        <div className="border p-4 rounded-lg shadow-md bg-white">
-            <img
-                src={offer?.plan?.coverImage}
-                alt={offer?.plan?.title?.en}
-                className="w-full h-32 object-cover rounded mb-4"
-            />
-            <h3 className="text-lg font-bold mb-2">{offer?.plan?.title?.en}</h3>
-            <p className="text-sm text-gray-600">
-                Coupon Code: {offer?.couponCode}
-            </p>
-            <p className="text-sm text-gray-600">
-                Start Date: {offer?.startingDate}
-            </p>
-            <p className="text-sm text-gray-600">
-                End Date: {offer?.endingDate}
-            </p>
-            <p className="text-sm text-gray-600">
-                Child Discount:{" "}
-                {offer.childDiscountType === "percentage"
-                    ? `${offer?.childDiscountPrice}%`
-                    : `${offer?.childDiscountPrice} Qar`}
-            </p>
-            <p className="text-sm text-gray-600">
-                Adult Discount:{" "}
-                {offer.adultDiscountType === "percentage"
-                    ? `${offer?.adultDiscountPrice}%`
-                    : `${offer?.adultDiscountPrice} Qar`}
-            </p>
-            <p className="text-sm text-gray-600">
-                Limit per User: {offer?.limit}
-            </p>
+  return (
+    <div className="border p-4 rounded-lg shadow-md bg-white">
+      <img
+        src={offer?.plan?.coverImage}
+        alt={offer?.plan?.title?.en}
+        className="w-full h-32 object-cover rounded mb-4"
+      />
+      <h3 className="text-lg font-bold mb-2">{offer?.plan?.title?.en}</h3>
+      <p className="text-sm text-gray-600">Coupon Code: {offer?.couponCode}</p>
+      <p className="text-sm text-gray-600">Start Date: {offer?.startingDate}</p>
+      <p className="text-sm text-gray-600">End Date: {offer?.endingDate}</p>
+      <p className="text-sm text-gray-600">
+        Child Discount:{" "}
+        {offer.childDiscountType === "percentage"
+          ? `${offer?.childDiscountPrice}%`
+          : `${offer?.childDiscountPrice} Qar`}
+      </p>
+      <p className="text-sm text-gray-600">
+        Adult Discount:{" "}
+        {offer.adultDiscountType === "percentage"
+          ? `${offer?.adultDiscountPrice}%`
+          : `${offer?.adultDiscountPrice} Qar`}
+      </p>
+      <p className="text-sm text-gray-600">Limit per User: {offer?.limit}</p>
 
-            {/* Status Button */}
-            <button
-                className={`mt-4 px-4 py-2 rounded ${statusClass}`}
-                onClick={() => handleStatus(offer._id)}
-            >
-                {offer.status === "active" ? "Active" : "Canceled"}
-            </button>
-        </div>
-    );
+      {/* Status Button */}
+      <div className="flex justify-between items-center mt-4">
+        <button
+          className={`px-4 py-2 rounded ${statusClass}`}
+          onClick={() => handleStatus(offer._id)}
+        >
+          {offer.status === "active" ? "Active" : "Canceled"}
+        </button>
+        <button onClick={() => handleRemove(offer._id)}>
+          <BiTrash size={24} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default OfferCard;
--- src/pages/Admin/Offers/index.jsx
+++ src/pages/Admin/Offers/index.jsx
@@ -3,32 +3,48 @@ import CreateOfferCard from "./CreateOfferCard";
 import OfferCard from "./OfferCard";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const Offers = () => {
-    const BASE_URL = import.meta.env.VITE_BASE_URL; // Make sure to set your BASE_URL properly
-    const [isOpen, setIsOpen] = useState(false);
-    const [plans, setPlans] = useState([]);
-    const [offers, setOffers] = useState([]);
-    const mainUser = useSelector((state) => state.user.user);
-    const [loading, setLoading] = useState(false);
+  const BASE_URL = import.meta.env.VITE_BASE_URL; // Make sure to set your BASE_URL properly
+  const [isOpen, setIsOpen] = useState(false);
+  const [plans, setPlans] = useState([]);
+  const [offers, setOffers] = useState([]);
+  const mainUser = useSelector((state) => state.user.user);
+  const [loading, setLoading] = useState(false);
 
-    const handleStatus = async (id) => {
-        if (mainUser.role !== "super-admin") return;
-        try {
-            const res = await axios.put(BASE_URL + "/offers/" + id);
-            const updatedOffer = res.data.data.offer;
+  const handleStatus = async (id) => {
+    if (mainUser.role !== "super-admin") return;
+    try {
+      const res = await axios.put(BASE_URL + "/offers/" + id);
+      const updatedOffer = res.data.data.offer;
 
-            setOffers((prevOffers) =>
-                prevOffers.map((offer) =>
-                    offer._id === updatedOffer._id ? updatedOffer : offer
-                )
-            );
-        } catch (err) {
-            console.log(err);
-        }
-    };
+      setOffers((prevOffers) =>
+        prevOffers.map((offer) =>
+          offer._id === updatedOffer._id ? updatedOffer : offer
+        )
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    const handleSubmit = async (
+  const handleSubmit = async (
+    couponCode,
+    startingDate,
+    endingDate,
+    childDiscountType,
+    adultDiscountType,
+    childDiscountPrice,
+    adultDiscountPrice,
+    selectedPlan,
+    limit
+  ) => {
+    setLoading(true);
+    if (mainUser.role !== "super-admin") return;
+
+    try {
+      const res = await axios.post(BASE_URL + "/offers", {
         couponCode,
         startingDate,
         endingDate,
@@ -36,88 +52,97 @@ const Offers = () => {
         adultDiscountType,
         childDiscountPrice,
         adultDiscountPrice,
-        selectedPlan,
-        limit
-    ) => {
-        setLoading(true);
-        if (mainUser.role !== "super-admin") return;
+        plan: selectedPlan,
+        limit,
+      });
 
-        try {
-            const res = await axios.post(BASE_URL + "/offers", {
-                couponCode,
-                startingDate,
-                endingDate,
-                childDiscountType,
-                adultDiscountType,
-                childDiscountPrice,
-                adultDiscountPrice,
-                plan: selectedPlan,
-                limit,
-            });
+      setOffers((prev) => [...prev, ...res.data.data.offers]);
+      setIsOpen(false);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
 
-            setOffers((prev) => [...prev, ...res.data.data.offers]);
-            setIsOpen(false);
-            setLoading(false);
-        } catch (err) {
-            setLoading(false);
+      console.log(err);
+    }
+  };
 
-            console.log(err);
-        }
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(BASE_URL + "/offers");
+        setOffers(response.data.data.offers);
+
+        const res = await axios.get(BASE_URL + "/plans/plan-titles");
+        setPlans(res.data.data.plans);
+      } catch (error) {
+        console.log(error);
+      }
     };
+    getData();
+  }, []);
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const response = await axios.get(BASE_URL + "/offers");
-                setOffers(response.data.data.offers);
+  const handleRemove = async (id) => {
+    try {
+      if (id) {
+        // Make delete request to the server
+        const res = await axios.delete(`${BASE_URL}/offers/${id}`);
 
-                const res = await axios.get(BASE_URL + "/plans/plan-titles");
-                setPlans(res.data.data.plans);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        getData();
-    }, []);
+        // Show success message
+        toast.success("Coupon deleted successfully!", {
+          theme: "dark",
+        });
 
-    return (
-        <div>
-            {/* Button to open the CreateOffer component */}
-            {mainUser && mainUser.role === "super-admin" && (
-                <button
-                    onClick={() => setIsOpen(true)}
-                    className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
-                >
-                    Create offer
-                </button>
-            )}
+        // Update the state to remove the deleted cruise from the UI
+        setOffers((prevCategories) =>
+          prevCategories.filter((category) => category._id !== id)
+        );
+      }
+    } catch (error) {
+      // Show error message
+      toast.error("Failed to delete Cruise.", { theme: "dark" });
+      console.error(error);
+    }
+  };
 
-            {/* Render the OfferCard for each offer in the data */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {offers.map((offer, index) => (
-                    <OfferCard
-                        key={index}
-                        offer={offer}
-                        handleStatus={handleStatus}
-                    />
-                ))}
-            </div>
+  return (
+    <div>
+      {/* Button to open the CreateOffer component */}
+      {mainUser && mainUser.role === "super-admin" && (
+        <button
+          onClick={() => setIsOpen(true)}
+          className="bg-blue-500 text-white px-4 py-2 rounded mb-4"
+        >
+          Create offer
+        </button>
+      )}
 
-            {/* Conditionally render the CreateOffer component as a modal */}
-            {isOpen && (
-                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
-                        <CreateOfferCard
-                            closeModal={() => setIsOpen(false)}
-                            plans={plans}
-                            handleSubmit={handleSubmit}
-                            loading={loading}
-                        />
-                    </div>
-                </div>
-            )}
+      {/* Render the OfferCard for each offer in the data */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {offers.map((offer, index) => (
+          <OfferCard
+            key={index}
+            offer={offer}
+            handleStatus={handleStatus}
+            handleRemove={handleRemove}
+          />
+        ))}
+      </div>
+
+      {/* Conditionally render the CreateOffer component as a modal */}
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
+            <CreateOfferCard
+              closeModal={() => setIsOpen(false)}
+              plans={plans}
+              handleSubmit={handleSubmit}
+              loading={loading}
+            />
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Offers;
