1f73c37206dbe7a41992408c6501facbfd6557c6
Rushwan07 <rushdeveloper07@gmail.com>
2024-10-17 15:42:18 +0530

    tour page image bugg fixed

--- src/pages/Tours/TourCard.jsx
+++ src/pages/Tours/TourCard.jsx
@@ -10,9 +10,17 @@ const TourCard = ({ image, description, title, link }) => {
       className="h-[500px]  bg-cover bg-center relative overflow-hidden group"
       style={{ backgroundImage: `url(${image})` }}
     >
-      <h3 className="bottom-10 absolute left-10  font-bold text-white text-2xl">
+   
+      <div className="relative h-full w-full">
+        <img
+          src={image}
+          className="w-full h-full bg-cover object-cover"
+          alt=""
+        />
+          <h3 className="bottom-10 absolute left-10  font-bold text-white text-2xl">
         {title}
       </h3>
+      </div>
       <div className="flex flex-col text-white justify-center items-start  p-10 absolute bg-custom-yellow-light inset-0 translate-y-full group-hover:translate-y-0 duration-500">
         <p className="text-lg  ">{description}</p>
         <Link to={`/tours/${link}`}>
