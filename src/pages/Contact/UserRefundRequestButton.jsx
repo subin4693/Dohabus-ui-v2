a231e92fbe6354d4249fedcc4cd9a230686f03bf
Melvin <melvinmps11301@gmail.com>
2025-03-15 12:42:56 +0300

    updated contact

---
+++ src/pages/Contact/UserRefundRequestButton.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import UserRefundRequestPopup from "./UserRefundRequestPopup";
+
+export default function UserRefundRequestButton() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  return (
+    <div className="mt-8">
+      <button
+        type="button"
+        onClick={() => setShowPopup(true)}
+        className="w-full py-3 bg-custom-yellow hover:bg-black hover:text-white rounded-full text-lg font-semibold transition duration-300"
+      >
+        Cancel Existing Booking?
+      </button>
+      {showPopup && (
+        <UserRefundRequestPopup onClose={() => setShowPopup(false)} />
+      )}
+    </div>
+  );
+}
